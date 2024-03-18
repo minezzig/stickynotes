7c30b5fda48212034c8f3d0b16afbe22c6a58905
Greg <gregoryminezzi@gmail.com>
2024-03-18 16:44:04 +0100

    implement env and concurrent

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import { createNote } from "./utils/api";
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,4 @@
-// const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:4000";
-const API_BASE_URL = "http://localhost:4000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
 
 const headers = new Headers();
 headers.append("Content-Type", "application/json");
