c1b42f5371bfc302d327095485876fdd42a760ad
Greg <gregoryminezzi@gmail.com>
2024-03-19 14:38:09 +0100

    edit vercel.json file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
-import {neon} from "@neondatabase/serverless";
-import {drizzle} from "drizzle-orm/neon-http";
-import {app} from "./app.js";
-import 'dotenv/config';
-import {notes} from "./db/schema.js"
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { app } from "./app.js";
+import "dotenv/config";
+import { notes } from "./db/schema.js";
 
 const sql = neon(process.env.DRIZZLE_DATABASE_URL);
 export const db = drizzle(sql);
@@ -11,12 +11,10 @@ export const db = drizzle(sql);
 //   const data = await db.select().from(notes);
 //   console.log(data)
 // }
-  // result();
-const {PORT = 4000} = process.env;
+// result();
+const { PORT = 4000 } = process.env;
 //const PORT = 4000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}. Yay it works!`);
-
 });
-
