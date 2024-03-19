35d259cf1c7b74ceba9a4f79ce851b449b272809
Greg <gregoryminezzi@gmail.com>
2024-03-19 15:30:37 +0100

    revert vercel.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,11 @@ import { drizzle } from "drizzle-orm/neon-http";
 import app from "./app.js";
 import "dotenv/config";
 
+// initialize Neon database connection and export for use in notes.service
 const sql = neon(process.env.DRIZZLE_DATABASE_URL);
 export const db = drizzle(sql);
 
+// set up express zpp;
 const { PORT = 4000 } = process.env;
 
 app.listen(PORT, () => {
