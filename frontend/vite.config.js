4c11c6d3c93400df806535276a000d2a08d440ee
Greg <gregoryminezzi@gmail.com>
2024-03-20 11:37:15 +0100

    revert vite.config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    __APP_ENV__: process.env.VITE_API_BASE_URL,
-  }
 })
