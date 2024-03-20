480be763e4ebc76cf2908e22ecd633f83802d295
Greg <gregoryminezzi@gmail.com>
2024-03-20 11:32:46 +0100

    edit vite.config file

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 //const API_BASE_URL = import.meta.env.API_BASE_URL || "http://localhost:4000";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
-console.log("URL",API_BASE_URL)
+//console.log("URL",API_BASE_URL)
 // set up header or each fetch request
 const headers = new Headers();
 headers.append("Content-Type", "application/json");
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_ENV__: process.env.VITE_API_BASE_URL,
+  },
 })
